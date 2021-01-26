962dc79c6579607a56a1ef9ef1b769a0ec7d067d
ThamsenBorges <54758338+ThamsenBorges@users.noreply.github.com>
2021-01-25 23:16:53 -0700

    hw2

---
+++ HW-2/README.md
@@ -0,0 +1,1 @@
+I worked with JS a bit for my final project of coding 2 so nothing was very new to me. I may have misunderstood some of this homework I am not positive.
---
+++ HW-2/main.js
@@ -0,0 +1,8 @@
+// My name is Thamsen Borges
+// My 3 favorite websites are Youtube, Twitch, and Twitter
+/*
+My 3 favorite games right now are League of Legends, Valorant, and Warzone
+My 3 favortie artists are Galantis, Kid Cudi, and Old Kanye
+*/
+
+console.log("My name is Thamsen Borges My 3 favorite websites are Youtube, Twitch, and Twitter My 3 favorite games right now are League of Legends, Valorant, and Warzone My 3 favortie artists are Galantis, Kid Cudi, and Old Kanye");
