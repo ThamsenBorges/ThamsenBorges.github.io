a0a69ea43f5fb0f81577ece03bd43334c199bb89
ThamsenBorges <54758338+ThamsenBorges@users.noreply.github.com>
2021-02-22 23:22:18 -0700

    Update main.js

--- HW-5/scripts/main.js
+++ HW-5/scripts/main.js
@@ -18,7 +18,7 @@ function printBlanks() {
 }
 
 function createRandomImageArray() {
-    var actualImagePath = ["images/mixed.jpg", "images/yuna.jpg", "images/black.jpg", "images/red.jpg", "images/white.jpg"];
+    var actualImagePath = ["images/mixed.jpg", "images/yunaa.jpg", "images/black.jpg", "images/red.jpg", "images/white.jpg"];
     var count = [0, 0, 0, 0, 0];
 
     while (actualImages.length < 10) {
